2aff19c0b7364c3a2f5c6491e672d5246b3a3ef7
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2022-04-24 19:25:45 +0500

    .

--- pak_charity/lib/screen/home/components/view_details_sheet.dart
+++ pak_charity/lib/screen/home/components/view_details_sheet.dart
@@ -5,18 +5,56 @@ class ViewDetailSheet extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AppBar(
-            leading:IconButton(onPressed:(){},icon:Icon(Icons.arrow_back_ios,),),
-          backgroundColor: Colors.transparent,
-          leadingWidth: 100,
-          elevation: 0.0,
-          title: const Text('Project Title'),
-          centerTitle: true,
-        ),
-      ],
-    );
+    return Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.filter_1),
+                  onPressed: () {
+                    // Do something
+                  }
+                ),
+                expandedHeight: 220.0,
+                floating: true,
+                pinned: true,
+                snap: true,
+                elevation: 50,
+                backgroundColor: Colors.pink,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text('Title',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.network(
+                      'https://images.pexels.com/photos/443356/pexels-photo-443356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                      fit: BoxFit.cover,
+                    )
+                ),
+              ),
+              new SliverList(
+                  delegate: new SliverChildListDelegate(_buildList(50))
+              ),
+            ],
+          ),
+        
+      );
+    }
+  
+    List _buildList(int count) {
+      List<Widget> listItems = List();
+  
+      for (int i = 0; i < count; i++) {
+        listItems.add(new Padding(padding: new EdgeInsets.all(20.0),
+            child: new Text(
+                'Item ${i.toString()}',
+                style: new TextStyle(fontSize: 25.0)
+            )
+        ));
+      }
+  
+      return listItems;
+    }
   }
-}
