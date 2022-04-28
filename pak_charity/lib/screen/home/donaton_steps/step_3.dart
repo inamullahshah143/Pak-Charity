5ec5033c373e9a4818b99e9a222e6d7ec07db868
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2022-04-28 23:33:36 +0500

    .

--- pak_charity/lib/screen/home/donaton_steps/step_3.dart
+++ pak_charity/lib/screen/home/donaton_steps/step_3.dart
@@ -19,28 +19,29 @@ CoolStep step3(donationMoney, paymentMethod) {
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.all(25),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: AppColor.secondary,
-            boxShadow: [
-              BoxShadow(
-                color: AppColor.fonts,
-                blurRadius: 1.0,
-                spreadRadius: 1.0,
-                offset: const Offset(0, 0),
-              ),
-            ],
-          ),
-          child: Text(
-            donationMoney,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        // Container(
+          
+        //   padding: const EdgeInsets.all(25),
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(10.0),
+        //     color: AppColor.secondary,
+        //     boxShadow: [
+        //       BoxShadow(
+        //         color: AppColor.fonts,
+        //         blurRadius: 1.0,
+        //         spreadRadius: 1.0,
+        //         offset: const Offset(0, 0),
+        //       ),
+        //     ],
+        //   ),
+        //   child: Text(
+        //     donationMoney.toString(),
+        //     style: const TextStyle(
+        //       fontSize: 18,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
@@ -52,28 +53,28 @@ CoolStep step3(donationMoney, paymentMethod) {
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.all(25),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: AppColor.secondary,
-            boxShadow: [
-              BoxShadow(
-                color: AppColor.fonts,
-                blurRadius: 1.0,
-                spreadRadius: 1.0,
-                offset: const Offset(0, 0),
-              ),
-            ],
-          ),
-          child: Text(
-            donationMoney,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        // Container(
+        //   padding: const EdgeInsets.all(25),
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(10.0),
+        //     color: AppColor.secondary,
+        //     boxShadow: [
+        //       BoxShadow(
+        //         color: AppColor.fonts,
+        //         blurRadius: 1.0,
+        //         spreadRadius: 1.0,
+        //         offset: const Offset(0, 0),
+        //       ),
+        //     ],
+        //   ),
+        //   child: Text(
+        //     paymentMethod.toString(),
+        //     style: const TextStyle(
+        //       fontSize: 18,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
@@ -85,28 +86,28 @@ CoolStep step3(donationMoney, paymentMethod) {
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.all(25),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: AppColor.secondary,
-            boxShadow: [
-              BoxShadow(
-                color: AppColor.fonts,
-                blurRadius: 1.0,
-                spreadRadius: 1.0,
-                offset: const Offset(0, 0),
-              ),
-            ],
-          ),
-          child: Text(
-            donationMoney,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        // Container(
+        //   padding: const EdgeInsets.all(25),
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(10.0),
+        //     color: AppColor.secondary,
+        //     boxShadow: [
+        //       BoxShadow(
+        //         color: AppColor.fonts,
+        //         blurRadius: 1.0,
+        //         spreadRadius: 1.0,
+        //         offset: const Offset(0, 0),
+        //       ),
+        //     ],
+        //   ),
+        //   child: Text(
+        //     donationMoney.toString(),
+        //     style: const TextStyle(
+        //       fontSize: 18,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
       ],
     ),
     validation: () {
